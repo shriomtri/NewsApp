c0e7c8b3d989e2df1e52de1576cb933fcd477acd
Shriom <shriomtripathi33@gmail.com>
2017-11-23 13:19:40 +0530

    Updated the UI of list_item with cardView

--- app/src/main/java/com/example/joker/newsapp/MainActivity.java
+++ app/src/main/java/com/example/joker/newsapp/MainActivity.java
@@ -11,6 +11,7 @@ import android.preference.PreferenceManager;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.AsyncTaskLoader;
 import android.support.v4.content.Loader;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -55,6 +56,11 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setTitle("TopHeadLines");
+
 
 
         //get default shared preference instance
