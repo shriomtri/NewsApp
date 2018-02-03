c0ebf22e2c1d8eb426af0f9ea1a81aacefc893fa
Shriom <shriomtripathi33@gmail.com>
2018-02-03 16:08:48 +0530

    Change the background color of slide 1,2 and 3

--- app/src/main/java/com/example/joker/newsapp/WelcomeActivity.java
+++ app/src/main/java/com/example/joker/newsapp/WelcomeActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.media.Image;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.PagerAdapter;
@@ -15,29 +16,30 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.joker.newsapp.Utils.HttpHandler;
 
 public class WelcomeActivity extends AppCompatActivity {
 
     private ViewPager ImageViewPager;
     private int[] layouts;
-    private Button btnSkip,btnNext;
+    private Button btnSkip, btnNext;
+    private boolean isInternet = false;
+    private Toast toast;
+    private SharedPreferences pref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        SharedPreferences pref = getSharedPreferences("First_Time_Launched", Context.MODE_PRIVATE);
-        if(pref.getBoolean("activity_executed",false)){
-            Intent intent = new Intent(WelcomeActivity.this,MainActivity.class);
+        pref = getSharedPreferences("First_Time_Launched", Context.MODE_PRIVATE);
+        if (pref.getBoolean("activity_executed", false)) {
+            Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
             startActivity(intent);
             finish();
-        }else{
-
-            SharedPreferences.Editor editor = pref.edit();
-            editor.putBoolean("activity_executed",true);
-            editor.apply();
-
         }
+
 
         if (Build.VERSION.SDK_INT >= 21) {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
@@ -49,7 +51,7 @@ public class WelcomeActivity extends AppCompatActivity {
 
         ImageViewPager = findViewById(R.id.pager);
         TabLayout tabLayout = findViewById(R.id.tabDots);
-        tabLayout.setupWithViewPager(ImageViewPager,true);
+        tabLayout.setupWithViewPager(ImageViewPager, true);
         btnSkip = findViewById(R.id.btn_skip);
         btnNext = findViewById(R.id.btn_next);
 
@@ -115,18 +117,25 @@ public class WelcomeActivity extends AppCompatActivity {
     }
 
     private void launchHomeScreen() {
-        startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
-        finish();
+
+        if (toast != null) {
+            toast.cancel();
+        }
+
+        new CheckNetwork().execute("https://www.google.com");
+        toast = Toast.makeText(getApplicationContext(), "Please wait...", Toast.LENGTH_LONG);
+        toast.show();
+
     }
 
-    public class MyViewPagerAdapter extends PagerAdapter{
+    public class MyViewPagerAdapter extends PagerAdapter {
 
         private LayoutInflater layoutInflater;
 
         @Override
         public Object instantiateItem(ViewGroup container, int position) {
             layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View view = layoutInflater.inflate(layouts[position],container,false);
+            View view = layoutInflater.inflate(layouts[position], container, false);
             container.addView(view);
             return view;
         }
@@ -145,6 +154,40 @@ public class WelcomeActivity extends AppCompatActivity {
         public void destroyItem(ViewGroup container, int position, Object object) {
             View v = (View) object;
             container.removeView(v);
+        }
+    }
+
+    public class CheckNetwork extends AsyncTask<String, Void, String> {
+
+
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return HttpHandler.makeServiceCall(strings[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            if (toast != null) {
+                toast.cancel();
+            }
+
+            if (s == null || s.length() == 0) {
+                toast = Toast.makeText(getApplicationContext(), "Please connect to internet", Toast.LENGTH_SHORT);
+                toast.show();
+            } else {
+
+                SharedPreferences.Editor editor = pref.edit();
+                editor.putBoolean("activity_executed", true);
+                editor.apply();
+
+                startActivity(new Intent(WelcomeActivity.this,MainActivity.class));
+                finish();
+            }
+
+
         }
     }
 }
